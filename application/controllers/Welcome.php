e28d89b3e3d12eaae5c21917b8bc5c6404739731
Dejack <bchen1104@gmail.com>
2016-09-25 15:27:11 -0700

    finished Basic to Good Website Conversion (first mate) Signed-off-by:Dejack <bchen1104@gmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -17,7 +17,28 @@ class Welcome extends Application {
      * @see https://codeigniter.com/user_guide/general/urls.html
      */
     public function index() {
-        // $this->load->view('welcome');	
+
+        $pix = $this->images->newest();
+        //die(var_dump($pix));
+        // $this->load->view('welcome');
+
+	foreach ($pix as $picture) {
+		$cells[] = $this->parser->parse('_cell', (array) $picture, true);
+	}
+	
+	// prime the table class
+	$this->load->library('table');
+	$parms = array(
+		'table_open' => '<table class ="gallery">',
+		'cell_start' => '<td class="oneimage">',
+		'cell_alt_start' => '<td class="oneimage">'
+	);
+	$this->table->set_template($parms);
+	
+	// generate the table
+	$rows = $this->table->make_columns($cells, 3);
+	$this->data['thetable'] = $this->table->generate($rows);	
+
         $this->data['pagebody'] = 'welcome';
         $this->render();
     }
---
+++ application/models/Images.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Images
+ *
+ * @author Ben
+ * @version 24/09/2016
+ * 
+ */
+class Images extends CI_Model{
+    // constructor
+    function __construct() {
+        parent:: __construct();
+    }
+    
+    // return all images, descending order by post date
+    function all() {
+        $this->db->order_by('id', 'desc');
+        $query = $this->db->get('images');
+        return $query->result_array();
+    }
+    
+    function newest() {
+	$this->db->order_by("id", "desc");
+	$this->db->limit(3);
+	$query = $this->db->get('images');
+	return $query->result_array();
+    }
+}
